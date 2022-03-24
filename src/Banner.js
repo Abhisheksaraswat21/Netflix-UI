ee507d5c5db0e41d3385a8ccf9c86cae65d57e0c
Abhishek Saraswat <77572876+Abhisheksaraswat21@users.noreply.github.com>
2022-03-24 23:36:16 +0530

    Update Banner.js

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react'
 import './Banner.css'
-//noew everytime we are accessing axios from our file not from global 
 import axios from "./axios";
 import requests from "./Requests";
 
@@ -16,8 +15,7 @@ useEffect(() => {
 async function fetchData() {
     const request = await axios.get(requests.fetchNetflixOriginals);
     setMovie(
-        //results is in the link of the fetchNetflixoriginals
-        //it generates a rnadom number between 0 to the totala number if movies in  the link 
+     
 
         request.data.results[Math.floor(Math.random() * request.data.results.length-1)
         ]
@@ -34,7 +32,6 @@ console.log(movie);
 
 
 const truncate = (string, n) => {
-    //this will cut the string when its length gets greater than n and puts ... there
     return string?.length > n ? string.substr(0,n-1) + '...' : string ;
 }
 
@@ -42,8 +39,7 @@ const truncate = (string, n) => {
     <header className='banner' style={
         {
             backgroundSize: "cover",
-            //https://image.tmdb.org/t/p/original/  this is generally a way of accessing the data from tmdb anf backdrop_path
-            //we get fromt the movie console.log
+            
             backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
             backgroundPosition: "center center",
             
@@ -53,7 +49,6 @@ const truncate = (string, n) => {
   <div className="banner__contents">
 
       <h1 className='banner__title'>
-          {/* //it means ki at times from link the name is coming in title or in name or in original name */}
 
           {movie?.title || movie?.name || movie?.original_name}
       </h1>
@@ -65,13 +60,11 @@ const truncate = (string, n) => {
 
 <h1 className="banner__description">
 {
-    //truncate is checking if the passed string is not greater than specified characters, if itis then it is putting ...
     truncate(movie?.overview,150)
 }
 </h1>
     </div>
 
-{/* this gives a nice fade at the end of this */}
     <div className="banner--fadeButton" />
     </header>
   )
